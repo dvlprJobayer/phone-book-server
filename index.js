ca346444a12587ce79dcbd11e160364348828a7b
dvlprJobayer <nesarahammed019@gmail.com>
2022-06-24 22:32:09 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// Make App
+const app = express();
+
+// MiddleWare
+app.use(cors());
+app.use(express.json());
+
+// Database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5bvzh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        console.log("db connected");
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+// Test Api
+app.get('/', (req, res) => {
+    res.send('Running Phone Book server');
+});
+
+// Listen App
+app.listen(port, () => {
+    console.log('Running server on', port);
+});
